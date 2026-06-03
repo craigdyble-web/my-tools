e1a186b9ddfbe255d50d7f8615c4f40c1708beae
craigdyble-web <craigdyble@googlemail.com>
2026-06-03 21:43:35 +0100

    Update sw.js

--- machine-movement/sw.js
+++ machine-movement/sw.js
@@ -1,1 +1,45 @@
 
+const CACHE_NAME = "machine-movement-offline-v1";
+const OFFLINE_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./exceljs.min.js",
+  "./template.xlsx",
+  "./template.xlsm"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return Promise.allSettled(OFFLINE_FILES.map(file => cache.add(file)));
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+      });
+    })
+  );
+});
